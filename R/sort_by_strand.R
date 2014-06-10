f36c98d3a176bb516d492c0b0839bb768ccc64aa
welch16 <welch@stat.wisc.edu>
2014-06-09 20:44:07 -0500

    Added sort_by_strand function

---
+++ R/sort_by_strand.R
@@ -0,0 +1,16 @@
+#' Sort the reads on a GRanges object depending on it's strand
+#'
+#' @param gr - A GRanges object
+#' @param stra - Unique strand of the object
+#' @return A GRanges object
+#' @export
+
+sort_by_strand <- function(gr,stra)
+{
+  if(stra == "-"){
+    gr = gr[sort(start(gr),index.return = TRUE)$ix]
+  }else{
+    gr = gr[sort(end(gr),index.return = TRUE)$ix]
+  }
+  return(gr)
+}
