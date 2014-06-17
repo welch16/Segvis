51dfc03dbba0386261c4da2441dd886b30b7d33f
welch16 <welch@stat.wisc.edu>
2014-06-17 12:25:50 -0500

    Added file format

--- R/create_profile.R
+++ R/create_profile.R
@@ -2,13 +2,14 @@
 #'
 #' @param regionName - Name of the profile object
 #' @param files - Character vector with files that contain the reads
+#' @param fileFormat - Character vector with the file format used
 #' @param fl - Numeric value of the fragment length used to extend the reads
 #' @param bw - Numeric value of the bandwidth used to smooth the coverage
 #' @export
 #' @return A profile object
 
-create_profile <- function(regionName,files,fl,bw)
+create_profile <- function(regionName,files,fileF,fl,bw)
 {
-  return(new("profile",name = regionName,bedfiles = files,
-    fragLen = fl, bandwidth = bw))             
+  return(new("profile",name = regionName,files = files,fileFormat = fileF,
+    fragLen = fl, bandwidth = bw))
 }
