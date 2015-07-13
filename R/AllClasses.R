eaff32528ecb3b727706c43a65dfd16f8b6bf3a0
welch16 <welch@stat.wisc.edu>
2015-07-12 19:09:10 -0500

    Modified the slot sections for class documentations and make the format inside the roxygen2 comments instead of using @slot roclets

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,15 +1,21 @@
-#' @title reads class description
+#' @importFrom methods setClass setGeneric setMethod setRefClass
+NULL
+
+#' reads class description
 #'
-#' @description Contains the reads obtained in a ChIP - seq experiment
-# separated by strand and then by chromosome.
+#' Contains the reads obtained in a ChIP - seq experiment separated by strand and then by chromosome. It has one component for each strand which are object of the data.table class with a match column to identify the regions
 #'
-#' @slot readsF List of data.table objects containing the reads of the
-#' ChIP - Seq experiment that have + strand.
+#' \section{Slots}{
+#'   \describe{
+#'     \item{\code{readsF}:}{List of data.table objects containing the reads of the ChIP - Seq experiment that have + strand.}
+#'     \item{\code{readsR}:}{List of data.table object containning the reads of the ChIP - Seq experiment that have - strand}
+#'   }
+#' }
+#' @seealso \code{\link{loadReads}}
 #'
-#' @slot readsR List of data.table object containning the reads of the
-#' ChIP - Seq experiment that have - strand
-#'
-#' @seealso \code{\link{readsF}}, \code{\link{readsR}} and \code{\link{loadReads}}
+#' @name reads-class
+#' @rdname reads-class
+#' @exportClass reads
 setClass("reads",
   representation(readsF = "list",readsR = "list"),
   prototype = prototype(readsF = list(),readsR = list()))
@@ -19,41 +25,31 @@ setValidity("reads",
     return(length(object@readsF) == length(object@readsR))
 })            
 
-#' @title segvis class description
+#' Segvis class description
 #'
-#' @description This object is the base class of the segvis package. It contains all the information necessary for the calculation of coverage curves
-#' 
-#' @slot name Character with the name of the profiles
+#' This object is the base class of the segvis package. It contains all the information necessary for the calculation of coverage curves.
 #'
-#' @slot regions GRanges object with the regions for which the profile want to be calcualted
-#'
-#' @slot file Character with the name of the file that contains the reads
-#'
-#' @slot maxBandwidth Numeric value with the maximum bandwidth accepted when smoothing profiles. Must be odd
-#' 
-#' @slot fragLen Numeric value with the fragment length to resize the reads (if it is zero then it doesn't resize the reads)
-#'
-#' @slot chr character vector, with the chromosomes to be considered
-#'
-#' @slot isPET logical, Indicates is the reads come from a PET experiments or a SET experiment
-#'
-#' @slot reads Reads object, which contains the reads used to build the profile separated by strand
-#'
-#' @slot profiles RleList - For each region, there is a Rle object
-#'
-#' @slot .haveRegions logical - Indicates if the object have the regions loaded
-#'
-#' @slot .haveReads logical - Indicates if the object have the reads loaded
-#'
-#' @slot .readsMatched logical - Indicates if the read have been matched to the regions
-#'
-#' @slot .coverageCalculated logical - Indicates if the coverage has been calculated]
-#'
-#' @seealso \code{\link{Segvis}}
-#'
-#' @exportClass segvis
-#'
+#' \section{Slots}{
+#'   \describe{
+#'     \item{\code{name}:}{Character with the name of the profiles}
+#'     \item{\code{regions}:}{GRanges object with the regions for which the profile want to be calcualted}
+#'     \item{\code{file}:}{Character with the name of the file that contains the reads}
+#'     \item{\code{maxBandwidth}:}{Numeric value with the maximum bandwidth accepted when smoothing profiles. Must be odd}
+#'     \item{\code{ fragLen}:}{Numeric value with the fragment length to resize the reads (if it is zero then it doesn't resize the reads)}
+#'     \item{\code{chr}:}{character vector, with the chromosomes to be considered}
+#'     \item{\code{isPET}:}{logical, Indicates is the reads come from a PET experiments or a SET experiment}
+#'     \item{\code{reads}:}{Reads object, which contains the reads used to build the profile separated by strand}
+#'     \item{\code{profiles}:}{RleList - For each region, there is a Rle object}
+#'     \item{\code{.haveRegions}:}{logical - Indicates if the object have the regions loaded}
+#'     \item{\code{.haveReads}:}{logical - Indicates if the object have the reads loaded}
+#'     \item{\code{.readsMatched}:}{logical - Indicates if the read have been matched to the regions}
+#'     \item{\code{.coverageCalculated}:}{logical - Indicates if the coverage has been calculated]}
+#'    }
+#' }
+#' @seealso \code{\link{buildSegvis}}
+#' @name segvis-class
 #' @rdname segvis-class
+#' @exportClass segvis
 setClass("segvis",
   representation(name = "character",
                  regions = "GRanges",
@@ -84,34 +80,28 @@ setClass("segvis",
                  .coverageCalculated = FALSE)
 )
 
-# Slots removed:
-# @slot fileFormat - Character with the file format used for the reads
-# @slot remChr - Character - Vector with the chromosomes to be ignored
-
-
 setValidity("segvis",
   function(object){
   return(object@fragLen >=0 & object@maxBandwidth >=1)
 })  
 
-#' @title segvis_block class description
+#' segvis_block class description
 #'
-#' @description Contains a data.table with the individual coverages for each region
+#' Contains a data.table with the individual coverages for each region
 #'
-#' @slot name Character with the name of the profiles
-#'
-#' @slot regions GRanges object with the regions of the individuals profiles
-#'
-#' @slot cover_table data.table with the individuals coverages for each region
-#'
-#' @slot bandwidth Numeric value used to smooth the individual profiles
-#'
-#' @slot normCont Numeric normalizing contanst
-#'
-#' @slot .isScaled Logical value representing if the profiles are scaled to \code{normConst}
-#'
+#' \section{Slots}{
+#'   \describe{
+#'    \item{\code{name}:}{Character with the name of the profiles}
+#'    \item{\code{regions}:}{GRanges object with the regions of the individuals profiles}
+#'    \item{\code{cover}:}{table data.table with the individuals coverages for each region}
+#'    \item{\code{bandwidth}:}{Numeric value used to smooth the individual profiles}
+#'    \item{\code{normCont}:}{Numeric normalizing contanst}
+#'    \item{\code{.isScaled}:}{Logical value representing if the profiles are scaled to \code{normConst}}
+#'    }
+#' }
+#' @seealso \code{\link{Segvis_block}}
 #' @exportClass segvis_block
-#'
+#' @name segvis_block-class
 #' @rdname segvis_block-class
 setClass("segvis_block",
   representation(name = "character",
@@ -128,9 +118,18 @@ setClass("segvis_block",
                  .isScaled = FALSE)
 )
 
-#' @title segvis_block_list class description
-#' @description Contains a list of segvis objects
+#' segvis_block_list class description
+#'
+#' Contains a list of \code{segvis_block} objects
+#'
+#' \section{Slots}{
+#'   \describe{
+#'    \item{\code{list}:}{A list of \code{segvis_block} objects or several \code{segvis_block} objects separated by commas}
+#'    }
+#' }
 #' @exportClass segvis_block_list
+#' @name segvis_block_list-class
+#' @rdname segvis_block_list-class
 setClass("segvis_block_list",         
   prototype = prototype(elementType = "segvis_block"),
   contains = "list"
