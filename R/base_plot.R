ffe663716f26e1d364ef12e5b55077fe059355ca
welch16 <welch@stat.wisc.edu>
2015-09-17 12:55:05 -0500

    fixed warnings of plot_data function

--- R/base_plot.R
+++ R/base_plot.R
@@ -1,6 +1,1 @@
 
-create_plot_data <- function(counts,name,coord)
-{
-  dt <- data.table(x=coord,y =counts,condition = name)
-  return(dt)
-}
--- R/base_segvis_block_list.R
+++ R/base_segvis_block_list.R
@@ -1,6 +1,14 @@
 ##' @import ggplot2
 NULL
 
+create_plot_data <- function(counts,name,coord)
+{
+  dt <- data.table(x=coord,y =counts,condition = name)
+  return(dt)
+}
+
+
+
 ##' @rdname plot_profiles-methods
 ##' @name plot_profiles-methods
 ##' @aliases plot_profiles,ANY-method
@@ -39,17 +47,45 @@ setMethods("plot_profiles",
     return(out)                  
 })
 
-##' @rdname plot_data-methods
-##' @name plot_data-methods
-##' @aliases plot_data,ANY-method
-##' @docType methods
-##' @exportMethod plot_data
-setMethods("plot_data",
-  signature = signature(object = "segvis_block_list",
-    condition = "ANY",coord = "numeric",FUN = "function",...="ANY",
-    mc = "numeric"),
-  definition = function(object,condition,coord,FUN,...,mc){
 
+##' Generates the data to be plotted
+##'
+##' This function generates a data.table with the profiles that would be in the profile plot
+##'
+##' @param object segvis_block_list object
+##'
+##' @param condition This is an expression used to filter the \code{segvis_block} objects, if there is no condition, then it doesn't filter the objects
+##'
+##' @param coord Numeric vector representing the coordinates in the a-axis of the plot, by default uses the natural index \code{1:ncol(segvis_block)}
+##'
+##' @param mc Numeber of cores used by parallel
+##'
+##' @param FUN function used to summarize the profiles
+##'
+##' @param ... Additional arguments of \code{FUN}
+##'
+##' @export
+##'
+##' @return A data.table object
+##'
+##' @rdname plot_data
+##' @name plot_data
+##' 
+##' @seealso \code{\link{segvis_block_list-class}},\code{\link{plot_profiles}}
+##'
+##' 
+##' @examples
+##' \dontrun{
+##'
+##'  plot_data(segvis_block_list,mc = 8, mean, trim = .05)
+##'
+##'  ## if the segvis_block_list have a column called row
+##'  plot_data(segvis_block_list,condition = row == 1,mc = 8,FUN = function(x)x)
+##'
+##' }
+plot_data <- function(object,condition,coord = NULL,mc,FUN,...)
+{
+  ## stopifnot(class(object) == "segvis_block_list")
   if(is.null(names(object))){nms = 1:length(object)}else{nms = names(object)}
   if(!missing(condition)){
     conds <- mclapply(object,.subset_logical,substitute(condition),mc.cores = mc)
@@ -63,7 +99,7 @@ setMethods("plot_data",
     stop("The supplied regions doesn't have the same width")
   }else      
   plot_width <- unique(unlist(widths))
-  if(missing(coord)){
+  if(is.null(coord)){
     coord <- 1:plot_width
   }
   profiles <- mclapply(subsets,summarize,FUN,...,mc.cores = mc)
@@ -71,5 +107,5 @@ setMethods("plot_data",
     SIMPLIFY = FALSE,mc.silent = TRUE,mc.cores = mc,mc.preschedule=TRUE)
   plot_data <- do.call(rbind,plot_data)    
  return(plot_data)
-})
+}
 
