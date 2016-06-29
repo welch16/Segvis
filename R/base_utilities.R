402522ce7c90c3be82f853a1d76dd39a888cc5da
welch16 <welch16@gmail.com>
2016-06-29 13:59:34 -0500

    added .readFileCover function which reads the aligned read files and converts them into a RleList by using coverage

--- R/base_utilities.R
+++ R/base_utilities.R
@@ -1,4 +1,29 @@
 
+##' @importFrom Rsamtools scanBamFlag
+##' @importFrom Rsamtools ScanBamParam
+NULL
+
+.readFileCover <- function(file,is_pet,frag_len,st = "*")
+{
+  stopifnot(st %in% c("+","-","*"))
+  if(is_pet){
+    pet_flag = scanBamFlag(isPaired = TRUE,isMinusStrand = (st == "-"))
+    param = ScanBamParam(flag = pet_flag)
+    cover = coverage(file,param  = param)
+  }else{
+    if(st != "*"){
+      flag = scanBamFlag(isMinusStrand = (st == "-"))
+      par = ScanBamParam(flag = flag)
+    }else{
+      par = NULL
+    }
+    reads = readGAlignments(file,param = par)
+    reads = resize(as(reads,"GRanges"),frag_len)
+    cover = coverage(reads)
+  }
+  return(cover)
+}
+
 .data.table.GRanges <- function(x)
 {
   dt <- data.table(seqnames = as.character( seqnames(x)),
@@ -12,7 +37,7 @@
   stopifnot(c("seqnames","start","end","strand") %in% names(x))
   return(GRanges(seqnames = x[,(seqnames)],
     ranges = .IRanges.data.table(x),
-    strand = x[,(strand)]))             
+    strand = x[,(strand)]))
 }
 
 .IRanges.data.table <- function(x)
